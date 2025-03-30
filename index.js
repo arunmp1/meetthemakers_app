3c6890e95689f3b60ee81be0bf64d3c10800a663
arunmp1 <arunmp192000@gmail.com>
2025-03-30 20:14:08 +0530

    Updated index

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.use(express.static(path.join(__dirname,'public')))
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs"); 
 
+app.get('/', (req, res) => {
+  res.render('index'); // Ensure index.ejs exists inside views/
+});
+
 
 app.get('/',isLoggedIn,async function(request,response){
   if(!request.user){
